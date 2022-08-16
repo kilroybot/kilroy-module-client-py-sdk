be5bab9b5253a66d849c56dc0027f5efabce5353
Sebastian Pietras <01133337@pw.edu.pl>
2022-08-16 15:26:37 +0200

    Moved to gRPC

--- kilroy_module_client_py_sdk/src/kilroy_module_client_py_sdk/client.py
+++ kilroy_module_client_py_sdk/src/kilroy_module_client_py_sdk/client.py
@@ -1,169 +1,429 @@
 import json
-from typing import AsyncIterable, Iterable, Type, TypeVar, Union
-
-from kilroy_module_py_shared import (
-    Config,
-    ConfigNotification,
-    ConfigSchema,
-    ConfigSetReply,
-    ConfigSetRequest,
-    FitPostsReply,
-    FitPostsRequest,
-    FitScoresReply,
-    FitScoresRequest,
-    GenerateReply,
-    GenerateRequest,
-    MetricsInfo,
-    MetricsNotification,
-    PostSchema,
-    Status,
-    StatusNotification,
-    StepReply,
-    StepRequest,
+from typing import (
+    Any,
+    AsyncIterable,
+    AsyncIterator,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Tuple,
+    Union,
 )
-from kilroy_ws_client_py_sdk import Client, JSON
-from kilroy_ws_py_shared import asyncify
-from pydantic import BaseModel
+from uuid import UUID
 
-M = TypeVar("M", bound=BaseModel)
+import betterproto
+from aiostream import stream
+from betterproto.grpc.grpclib_client import MetadataLike
+from grpclib.metadata import Deadline
+from kilroy_module_py_shared import (
+    FitPostsRequest,
+    FitPostsResponse,
+    FitScoresRequest,
+    FitScoresResponse,
+    GenerateRequest,
+    GenerateResponse,
+    GetConfigRequest,
+    GetConfigResponse,
+    GetConfigSchemaRequest,
+    GetConfigSchemaResponse,
+    GetMetadataRequest,
+    GetMetadataResponse,
+    GetMetricsConfigRequest,
+    GetMetricsConfigResponse,
+    GetPostSchemaRequest,
+    GetPostSchemaResponse,
+    GetStatusRequest,
+    GetStatusResponse,
+    Metadata,
+    PostScore,
+    RealPost,
+    SetConfigRequest,
+    SetConfigResponse,
+    Status,
+    StepRequest,
+    StepResponse,
+    WatchConfigRequest,
+    WatchConfigResponse,
+    WatchMetricsRequest,
+    WatchMetricsResponse,
+    WatchStatusRequest,
+    WatchStatusResponse,
+)
+
+from kilroy_module_client_py_sdk.metrics import MetricConfig, MetricData
 
 
-class ModuleClient:
-    def __init__(self, url: str, *args, **kwargs) -> None:
-        self._client = Client(url, *args, **kwargs)
-
-    async def _get(self, path: str, model: Type[M], **kwargs) -> M:
-        payload = await self._client.get(path, **kwargs)
-        return model.parse_obj(payload)
-
-    async def _subscribe(
-        self, path: str, model: Type[M], **kwargs
-    ) -> AsyncIterable[M]:
-        async for payload in self._client.subscribe(path, **kwargs):
-            yield model.parse_obj(payload)
-
-    async def _request(
+class ModuleServiceStub(betterproto.ServiceStub):
+    async def get_metadata(
         self,
-        path: str,
-        request: BaseModel,
-        reply_model: Type[M],
-        **kwargs,
-    ) -> M:
-        request_payload = json.loads(request.json())
-        reply_payload = await self._client.request(
-            path,
-            data=request_payload,
-            **kwargs,
+        get_metadata_request: "GetMetadataRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> "GetMetadataResponse":
+        return await self._unary_unary(
+            "/kilroy.module.v1alpha.ModuleService/GetMetadata",
+            get_metadata_request,
+            GetMetadataResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
         )
-        return reply_model.parse_obj(reply_payload)
 
-    async def _request_stream_out(
+    async def get_post_schema(
         self,
-        path: str,
-        request: BaseModel,
-        reply_model: Type[M],
-        **kwargs,
-    ) -> AsyncIterable[M]:
-        request_payload = json.loads(request.json())
-        async for payload in self._client.request_stream_out(
-            path,
-            data=request_payload,
-            **kwargs,
-        ):
-            yield reply_model.parse_obj(payload)
-
-    async def _request_stream_in(
-        self,
-        path: str,
-        requests: Union[Iterable[BaseModel], AsyncIterable[BaseModel]],
-        reply_model: Type[M],
-        **kwargs,
-    ) -> M:
-        async def _payloads(
-            requests: Union[Iterable[BaseModel], AsyncIterable[BaseModel]]
-        ) -> AsyncIterable[JSON]:
-            async for request in asyncify(requests):
-                yield json.loads(request.json())
-
-        reply_payload = await self._client.request_stream_in(
-            path,
-            data=_payloads(requests),
-            **kwargs,
+        get_post_schema_request: "GetPostSchemaRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> "GetPostSchemaResponse":
+        return await self._unary_unary(
+            "/kilroy.module.v1alpha.ModuleService/GetPostSchema",
+            get_post_schema_request,
+            GetPostSchemaResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
         )
-        return reply_model.parse_obj(reply_payload)
 
-    async def post_schema(self, **kwargs) -> PostSchema:
-        return await self._get("/post/schema", PostSchema, **kwargs)
-
-    async def status(self, **kwargs) -> Status:
-        return await self._get("/status", Status, **kwargs)
+    async def get_status(
+        self,
+        get_status_request: "GetStatusRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> "GetStatusResponse":
+        return await self._unary_unary(
+            "/kilroy.module.v1alpha.ModuleService/GetStatus",
+            get_status_request,
+            GetStatusResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
+        )
 
     async def watch_status(
-        self, **kwargs
-    ) -> AsyncIterable[StatusNotification]:
-        async for data in self._subscribe(
-            "/status/watch", StatusNotification, **kwargs
+        self,
+        watch_status_request: "WatchStatusRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> AsyncIterator["WatchStatusResponse"]:
+        async for response in self._unary_stream(
+            "/kilroy.module.v1alpha.ModuleService/WatchStatus",
+            watch_status_request,
+            WatchStatusResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
         ):
-            yield data
+            yield response
 
-    async def config(self, **kwargs) -> Config:
-        return await self._get("/config", Config, **kwargs)
+    async def get_config_schema(
+        self,
+        get_config_schema_request: "GetConfigSchemaRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> "GetConfigSchemaResponse":
+        return await self._unary_unary(
+            "/kilroy.module.v1alpha.ModuleService/GetConfigSchema",
+            get_config_schema_request,
+            GetConfigSchemaResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
+        )
 
-    async def config_schema(self, **kwargs) -> ConfigSchema:
-        return await self._get("/config/schema", ConfigSchema, **kwargs)
+    async def get_config(
+        self,
+        get_config_request: "GetConfigRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> "GetConfigResponse":
+        return await self._unary_unary(
+            "/kilroy.module.v1alpha.ModuleService/GetConfig",
+            get_config_request,
+            GetConfigResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
+        )
 
     async def watch_config(
-        self, **kwargs
-    ) -> AsyncIterable[ConfigNotification]:
-        async for data in self._subscribe(
-            "/config/watch", ConfigNotification, **kwargs
+        self,
+        watch_config_request: "WatchConfigRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> AsyncIterator["WatchConfigResponse"]:
+        async for response in self._unary_stream(
+            "/kilroy.module.v1alpha.ModuleService/WatchConfig",
+            watch_config_request,
+            WatchConfigResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
         ):
-            yield data
+            yield response
 
     async def set_config(
-        self, request: ConfigSetRequest, **kwargs
-    ) -> ConfigSetReply:
-        return await self._request(
-            "/config/set", request, ConfigSetReply, **kwargs
+        self,
+        set_config_request: "SetConfigRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> "SetConfigResponse":
+        return await self._unary_unary(
+            "/kilroy.module.v1alpha.ModuleService/SetConfig",
+            set_config_request,
+            SetConfigResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
         )
 
     async def generate(
-        self, request: GenerateRequest, **kwargs
-    ) -> AsyncIterable[GenerateReply]:
-        async for data in self._request_stream_out(
-            "/generate", request, GenerateReply, **kwargs
+        self,
+        generate_request: "GenerateRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> AsyncIterator["GenerateResponse"]:
+        async for response in self._unary_stream(
+            "/kilroy.module.v1alpha.ModuleService/Generate",
+            generate_request,
+            GenerateResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
         ):
-            yield data
+            yield response
 
     async def fit_posts(
         self,
-        requests: Union[
-            Iterable[FitPostsRequest],
-            AsyncIterable[FitPostsRequest],
+        fit_posts_request_iterator: Union[
+            AsyncIterable["FitPostsRequest"], Iterable["FitPostsRequest"]
         ],
-        **kwargs,
-    ) -> FitPostsReply:
-        return await self._request_stream_in(
-            "/fit/posts", requests, FitPostsReply, **kwargs
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> "FitPostsResponse":
+        return await self._stream_unary(
+            "/kilroy.module.v1alpha.ModuleService/FitPosts",
+            fit_posts_request_iterator,
+            FitPostsRequest,
+            FitPostsResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
         )
 
     async def fit_scores(
-        self, request: FitScoresRequest, **kwargs
-    ) -> FitScoresReply:
-        return await self._request(
-            "/fit/scores", request, FitScoresReply, **kwargs
+        self,
+        fit_scores_request: "FitScoresRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> "FitScoresResponse":
+        return await self._unary_unary(
+            "/kilroy.module.v1alpha.ModuleService/FitScores",
+            fit_scores_request,
+            FitScoresResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
         )
 
-    async def step(self, request: StepRequest, **kwargs) -> StepReply:
-        return await self._request("/step", request, StepReply, **kwargs)
+    async def step(
+        self,
+        step_request: "StepRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> "StepResponse":
+        return await self._unary_unary(
+            "/kilroy.module.v1alpha.ModuleService/Step",
+            step_request,
+            StepResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
+        )
 
-    async def metrics_info(self, **kwargs) -> MetricsInfo:
-        return await self._get("/metrics/info", MetricsInfo, **kwargs)
+    async def get_metrics_config(
+        self,
+        get_metrics_config_request: "GetMetricsConfigRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> "GetMetricsConfigResponse":
+        return await self._unary_unary(
+            "/kilroy.module.v1alpha.ModuleService/GetMetricsConfig",
+            get_metrics_config_request,
+            GetMetricsConfigResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
+        )
 
     async def watch_metrics(
-        self, **kwargs
-    ) -> AsyncIterable[MetricsNotification]:
-        async for data in self._subscribe(
-            "/metrics/watch", MetricsNotification, **kwargs
+        self,
+        watch_metrics_request: "WatchMetricsRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None,
+    ) -> AsyncIterator["WatchMetricsResponse"]:
+        async for response in self._unary_stream(
+            "/kilroy.module.v1alpha.ModuleService/WatchMetrics",
+            watch_metrics_request,
+            WatchMetricsResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
         ):
-            yield data
+            yield response
+
+
+class ModuleService:
+    def __init__(self, *args, **kwargs) -> None:
+        self._stub = ModuleServiceStub(*args, **kwargs)
+
+    async def get_metadata(self, *args, **kwargs) -> Metadata:
+        response = await self._stub.get_metadata(
+            GetMetadataRequest(), *args, **kwargs
+        )
+        return Metadata(key=response.key, description=response.description)
+
+    async def get_post_schema(self, *args, **kwargs) -> Dict[str, Any]:
+        response = await self._stub.get_post_schema(
+            GetPostSchemaRequest(), *args, **kwargs
+        )
+        return json.loads(response.schema)
+
+    async def get_status(self, *args, **kwargs) -> Status:
+        response = await self._stub.get_status(
+            GetStatusRequest(), *args, **kwargs
+        )
+        return response.status
+
+    async def watch_status(self, *args, **kwargs) -> AsyncIterator[Status]:
+        async for response in self._stub.watch_status(
+            WatchStatusRequest(), *args, **kwargs
+        ):
+            yield response.status
+
+    async def get_config_schema(self, *args, **kwargs) -> Dict[str, Any]:
+        response = await self._stub.get_config_schema(
+            GetConfigSchemaRequest(), *args, **kwargs
+        )
+        return json.loads(response.schema)
+
+    async def get_config(self, *args, **kwargs) -> Dict[str, Any]:
+        response = await self._stub.get_config(
+            GetConfigRequest(), *args, **kwargs
+        )
+        return json.loads(response.config)
+
+    async def watch_config(
+        self, *args, **kwargs
+    ) -> AsyncIterator[Dict[str, Any]]:
+        async for response in self._stub.watch_config(
+            WatchConfigRequest(), *args, **kwargs
+        ):
+            yield json.loads(response.config)
+
+    async def set_config(
+        self, config: Dict[str, Any], *args, **kwargs
+    ) -> "SetConfigResponse":
+        response = await self._stub.set_config(
+            SetConfigRequest(config=json.dumps(config)), *args, **kwargs
+        )
+        return json.loads(response.config)
+
+    async def generate(
+        self, quantity: int = 1, *args, **kwargs
+    ) -> AsyncIterator[Tuple[UUID, Dict[str, Any]]]:
+        async for response in self._stub.generate(
+            GenerateRequest(quantity=quantity), *args, **kwargs
+        ):
+            yield UUID(response.post.id), json.loads(response.post.content)
+
+    async def fit_posts(
+        self,
+        posts: Union[AsyncIterable[Dict[str, Any]], Iterable[Dict[str, Any]]],
+        *args,
+        **kwargs,
+    ) -> None:
+        async with stream.iterate(posts).stream() as posts:
+
+            async def to_requests():
+                async for post in posts:
+                    yield FitPostsRequest(
+                        post=RealPost(content=json.dumps(post))
+                    )
+
+            await self._stub.fit_posts(to_requests(), *args, **kwargs)
+
+    async def fit_scores(
+        self,
+        scores: Union[
+            AsyncIterable[Tuple[UUID, float]], Iterable[Tuple[UUID, float]]
+        ],
+        *args,
+        **kwargs,
+    ) -> None:
+        async with stream.iterate(scores).stream() as scores:
+            request = FitScoresRequest(
+                scores=[
+                    PostScore(id=str(uid), score=score)
+                    async for uid, score in scores
+                ]
+            )
+
+            await self._stub.fit_scores(request, *args, **kwargs)
+
+    async def step(self, *args, **kwargs) -> None:
+        await self._stub.step(StepRequest(), *args, **kwargs)
+
+    async def get_metrics_config(self, *args, **kwargs) -> List[MetricConfig]:
+        response = await self._stub.get_metrics_config(
+            GetMetricsConfigRequest(), *args, **kwargs
+        )
+
+        return [
+            MetricConfig(
+                id=metric.id,
+                label=metric.label,
+                config=json.loads(metric.config),
+            )
+            for metric in response.configs
+        ]
+
+    async def watch_metrics(
+        self, *args, **kwargs
+    ) -> AsyncIterator[MetricData]:
+        async for response in self._stub.watch_metrics(
+            WatchMetricsRequest(), *args, **kwargs
+        ):
+            yield MetricData(
+                metric_id=response.metric_id,
+                dataset_id=response.dataset_id,
+                data=json.loads(response.data),
+            )
