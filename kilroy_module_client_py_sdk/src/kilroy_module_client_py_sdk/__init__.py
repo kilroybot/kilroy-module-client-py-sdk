b4c71395cf9cbe8495162b095b7695af0cdb5dcf
Sebastian Pietras <01133337@pw.edu.pl>
2022-08-16 15:37:42 +0200

    Moved to `gRPC` (#10)
    Moved to gRPC

--- kilroy_module_client_py_sdk/src/kilroy_module_client_py_sdk/__init__.py
+++ kilroy_module_client_py_sdk/src/kilroy_module_client_py_sdk/__init__.py
@@ -1,27 +1,8 @@
-from kilroy_module_client_py_sdk.client import ModuleClient
-from kilroy_module_py_shared import (
-    Config,
-    ConfigNotification,
-    ConfigSchema,
-    ConfigSetReply,
-    ConfigSetRequest,
-    FitPostsReply,
-    FitPostsRequest,
-    PostScore,
-    FitScoresReply,
-    FitScoresRequest,
-    GenerateReply,
-    GenerateRequest,
-    MetricsInfo,
-    MetricsNotification,
-    PostSchema,
-    Status,
-    StatusNotification,
-    StepReply,
-    StepRequest,
-)
+from kilroy_module_client_py_sdk.client import ModuleServiceStub, ModuleService
+from kilroy_module_py_shared import *
 from kilroy_module_client_py_sdk.resources import (
     resource,
     resource_bytes,
     resource_text,
 )
+from kilroy_module_client_py_sdk.metrics import MetricConfig, MetricData
